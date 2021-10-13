d72ccf393cd65f005e52c4e3b277ca02e3fc5c5f
vishwpatel511 <vishwpatel511@gmail.com>
2021-10-13 12:41:04 -0400

    init commit

---
+++ src/main/java/CucumberwithPOM/CucumberwithPOM/App.java
@@ -0,0 +1,13 @@
+package CucumberwithPOM.CucumberwithPOM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
